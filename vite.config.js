dca80715c576b9c405f5032b358bb643acf32d88
srinidhiv2107 <srinidhi01nov@gmail.com>
2025-06-09 16:46:43 +0530

    Modified base and homepage keys

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: "/Wertetek/",
+  base: "/",
   plugins: [
     react(),
     tailwindcss(),
